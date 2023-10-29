abe60a4909f31fe90f6e19733b11100839ef5b68
Hoang Hung Anh <90163072+Codedr-edu@users.noreply.github.com>
2023-10-29 19:50:08 +0700

    Add files via upload

--- social_learning/models.py
+++ social_learning/models.py
@@ -16,7 +16,7 @@ class Bio(models.Model):
     grade = models.IntegerField()
     address = models.TextField()
     address_password = models.TextField()
-    wallet_passcode = models.CharField(max_length=6)
+    wallet_passcode = models.TextField()
     edu_rank = models.ForeignKey(Education_rank,on_delete=models.CASCADE,related_name="user_edu_rank")
     avatar = models.ImageField(upload_to="images/",null=True,blank=True)
     thumnail = models.ImageField(upload_to="images/",null=True,blank=True)
@@ -27,12 +27,6 @@ class Subject(models.Model):
     description = models.TextField()
     schoolable = models.IntegerField()
 
-class Comment_gigs(models.Model):
-    content = models.TextField()
-    image = models.ImageField(upload_to="images/",null=True,blank=True)
-    user = models.ForeignKey(Bio,on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-    reply = models.ForeignKey('self',on_delete=models.CASCADE,related_name="comment_gigs_reply")
 
 class Gigs(models.Model):
     title = models.CharField()
@@ -41,22 +35,32 @@ class Gigs(models.Model):
     image = models.ImageField(upload_to="images/",null=True,blank=True)
     education_rank = models.ForeignKey(Education_rank,on_delete=models.CASCADE,related_name="Education_rank")
     grade = models.IntegerField()
+    price = models.FloatField()
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE,related_name="subject_choose")
+    user = models.ForeignKey(Bio, on_delete=models.CASCADE, related_name="gigs_auth")
     book_include = models.CharField(max_length=1000)
     type_learn = models.CharField(max_length=1000)
     like = models.ManyToManyField(Bio)
     dislike = models.ManyToManyField(Bio)
-    comment = models.ForeignKey(Comment_gigs,on_delete=models.CASCADE,related_name="comment_gigs")
 
-class Answer(models.Model):
+class Comment_Post(models.Model):
     content = models.TextField()
-    image = models.ImageField(upload_to="images/",null=True,blank=True)
-    file = models.FileField(upload_to="files/",null=True,blank=True)
-    user = models.ForeignKey(Bio,on_delete=models.CASCADE,related_name="question_user_answer")
-    reply = models.ForeignKey('self',on_delete=models.CASCADE,related_name="answer_reply")
-    like = models.ManyToManyField(Bio)
-    dislike = models.ManyToManyField(Bio)
-    choosen = models.IntegerField()
+    user = models.ForeignKey(
+       Bio, related_name='comment_gigs_user', on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(
+        Gigs, related_name='comment_gigs', on_delete=models.CASCADE, null=True)
+    reply = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name="post_reply")
+    
+class join_cls(models.Model):
+    gig = models.ForeignKey(Gigs,on_delete=models.CASCADE,related_name="joined_gig")
+    student =  models.ForeignKey(Bio,on_delete=models.CASCADE,related_name="student_join")
+
+class Learn(models.Model):
+    check_stu = models.ForeignKey(join_cls,on_delete=models.CASCADE,related_name="check_stu")
+    cls_day = models.IntegerField()
     datetime = models.DateTimeField(auto_now_add=True)
+
 
 class Question(models.Model):
     title = models.CharField()
@@ -67,10 +71,21 @@ class Question(models.Model):
     subject = models.ForeignKey(Subject)
     education_rank = models.ForeignKey(Education_rank,on_delete=models.CASCADE,related_name="Education_rank_question")
     grade = models.IntegerField()
-    price = models.IntegerField()
+    price = models.FloatField()
     like = models.ManyToManyField(Bio)
     dislike = models.ManyToManyField(Bio)
-    answer = models.ForeignKey(Answer,on_delete=models.CASCADE,related_name="answer")
+    datetime = models.DateTimeField(auto_now_add=True)
+
+class Answer(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/",null=True,blank=True)
+    file = models.FileField(upload_to="files/",null=True,blank=True)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name="ques_select")
+    user = models.ForeignKey(Bio,on_delete=models.CASCADE,related_name="question_user_answer")
+    reply = models.ForeignKey('self',on_delete=models.CASCADE,related_name="answer_reply")
+    upvote = models.ManyToManyField(Bio)
+    downvote = models.ManyToManyField(Bio)
+    choosen = models.IntegerField()
     datetime = models.DateTimeField(auto_now_add=True)
 
 class Document(models.Model):
@@ -85,20 +100,31 @@ class Document(models.Model):
     dislike = models.ManyToManyField(Bio)
     datetime = models.DateTimeField(auto_now_add=True)
 
-class Comment_post(models.Model):
-    content = models.CharField(max_length=1000)
-    user = user = models.ForeignKey(Bio,on_delete=models.CASCADE,related_name="comment_post_auth")
-    datetime = models.DateTimeField(auto_now_add=True)
-    reply = models.ForeignKey('self',on_delete=models.CASCADE,related_name="comment_post_reply")
-
 class Post(models.Model):
     content = models.CharField(max_length=1000)
-    comment = models.ForeignKey(Comment_post,on_delete=models.CASCADE,related_name="comment_post")
     like = models.ManyToManyField(Bio)
     user = models.ForeignKey(Bio,on_delete=models.CASCADE,related_name="post_auth")
     dislike = models.ManyToManyField(Bio)
     datetime = models.DateTimeField(auto_now_add=True)
+
+class Comment_Post(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(
+       Bio, related_name='comment_post_user', on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(
+        Post, related_name='comment_post', on_delete=models.CASCADE, null=True)
+    reply = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name="post_reply")
+
+class payment_method(models.Model):
+    name = models.CharField()
+
     
+class Trade(models.Model):
+    value = models.FloatField()
+    payment_method = models.ForeignKey(payment_method)
+    done = models.CharField(max_length=10)
+    datetime = models.DateTimeField(auto_now_add=True)
 
 
     
